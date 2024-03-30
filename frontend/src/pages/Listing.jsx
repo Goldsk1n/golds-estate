4b9151ac20c7cf67e443e22408e96ecb0dbcc7b8
Denis Lednev <denzik252@gmail.com>
2024-03-30 20:19:11 +0200

    Added image slider to listing page

---
+++ frontend/src/pages/Listing.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+
+export default function Listing() {
+    SwiperCore.use([Navigation]);
+    const params = useParams();
+    const [listing, setListing] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(
+        (async () => {
+            try {
+                setLoading(true);
+                const res = await fetch(
+                    `/backend/listing/get/${params.listingId}`
+                );
+                const data = await res.json();
+                if (!data.success) {
+                    setError(true);
+                    setLoading(false);
+                    return;
+                }
+                setListing(data);
+                setLoading(false);
+                setError(false);
+            } catch (error) {
+                setError(true);
+                setLoading(false);
+            }
+        })(),
+        []
+    );
+
+    return (
+        <main>
+            {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
+            {error && (
+                <p className="text-center my-7 text-2xl">
+                    Something went wrong!
+                </p>
+            )}
+            {listing && !loading && !error && (
+                <div>
+                    <Swiper navigation>
+                        {listing.imageUrls.map((url) => (
+                            <SwiperSlide key={url}>
+                                <div
+                                    className="h-[550px]"
+                                    style={{ background: `url(${url}) center no-repeat` }}
+                                ></div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
+            )}
+        </main>
+    );
+}
